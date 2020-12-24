3b31a46d1d569f87349cc3b55e9e48c0c19db2a3
Mohammad Honravar <honarvar.info@gmail.com>
2020-12-25 00:43:08 +0330

    feat(index): add new `imports`

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import './common/config';
 import { bot } from './telegram-bot';
-import './bot';
+// import './bot';
 // import './login-bot';
+// import './send-message-to-channel';
+import './get-updates';
 
 bot.launch();

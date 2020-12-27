a3296f1d946b0fa3c9d7ead841c9a647a9069a4c
Mohammad Honravar <honarvar.info@gmail.com>
2020-12-28 02:21:59 +0330

    refactor(common/config): load `config.json` content instead of env file

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,25 +1,28 @@
-import dotenv from 'dotenv';
-import fs from 'fs';
+// import dotenv from 'dotenv';
+// import fs from 'fs';
 
 import { debug } from 'debug';
 debug.enable(process.env.DEBUG || 'app/*');
-const log = debug('app/config');
+// const log = debug('app/config');
 
-const loadDotEnv = (path: string): boolean => {
-  if (!fs.existsSync(path)) {
-    log(`Skip parsing ${path}, File not found.`);
-    return false;
-  }
-  // else
-  log(`Using ${path} to supply config environment variables`);
-  const dotenvResult = dotenv.config({ path });
-  log('%s parsed: %j', path, dotenvResult.parsed);
-  if (dotenvResult.error) {
-    log('%s error', path, dotenvResult.error);
-    return false;
-  }
-  // else
-  return true;
-};
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+export const config = require('../../config.json');
 
-loadDotEnv('.bot.env') || loadDotEnv('.bot.env.example');
+// const loadDotEnv = (path: string): boolean => {
+//   if (!fs.existsSync(path)) {
+//     log(`Skip parsing ${path}, File not found.`);
+//     return false;
+//   }
+//   // else
+//   log(`Using ${path} to supply config environment variables`);
+//   const dotenvResult = dotenv.config({ path });
+//   log('%s parsed: %j', path, dotenvResult.parsed);
+//   if (dotenvResult.error) {
+//     log('%s error', path, dotenvResult.error);
+//     return false;
+//   }
+//   // else
+//   return true;
+// };
+
+// loadDotEnv('.bot.env') || loadDotEnv('.bot.env.example');

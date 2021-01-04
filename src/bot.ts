1a93d84bfff8bb2d300e7e32c7f0c890088b581b
Mohammad Honravar <honarvar.info@gmail.com>
2021-01-04 03:57:59 +0330

    chore(bot): update some codes

--- src/bot.ts
+++ src/bot.ts
@@ -4,14 +4,29 @@ import { debug } from 'debug';
 const log = debug('app/bot');
 
 bot.start((ctx: Context) =>
-  ctx.reply(`Welcome To ${process.env.BOT_USERID} ❤😍💕`),
+  ctx.reply(`Welcome To ${process.env.BOT_USERID ?? 'Our bot'} ❤😍💕`),
 );
 
 bot.on('photo', (ctx: Context) => {
   log('Photo details: %o', ctx.message);
 });
 
+bot.on('video_note', (ctx: Context) => {
+  log('Video Note details: %o', ctx.message);
+});
+
+bot.on('video', (ctx: Context) => {
+  log('Video Note details: %o', ctx.message);
+});
+
+bot.on('audio', (ctx: Context) => {
+  log('Voice details: %o', ctx.message);
+});
+
 bot.on('message', (ctx: Context) => {
+  if (ctx.message == null || ctx.message.text == null || ctx.chat == null) {
+    return;
+  }
   log('Message is: %s', ctx.message.text);
   bot.telegram.sendChatAction(ctx.chat.id, 'typing');
 
